86b38137ce28bd74b268eaadc43fdee70eb361b4
Lodkahs <omalojiu@gmail.com>
2023-08-21 11:06:43 +0300

    Initial Commit

---
+++ Ukraine_russia_War_Dataset_2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ukraine_russia_War_Dataset_2022
+//
+//  Created by Andrew  on 8/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ukraine_russia_War_Dataset_2022/Ukraine_russia_War_Dataset_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Ukraine_russia_War_Dataset_2022App.swift
+//  Ukraine_russia_War_Dataset_2022
+//
+//  Created by Andrew  on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Ukraine_russia_War_Dataset_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
